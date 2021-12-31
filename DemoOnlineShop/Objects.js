35c5b4ef19de3ddf3134086ce12583c4837047a4
Wild1M <82161909+Wild1M@users.noreply.github.com>
2021-12-31 04:34:35 +0200

    Demo

---
+++ DemoOnlineShop/Objects.js
@@ -0,0 +1,643 @@
+var saved_script_objects={
+	"Phones": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Phones",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[contains(@onclick,'phone')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/"
+	},
+	"Laptops": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Laptops",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[contains(@onclick,'notebook')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/"
+	},
+	"Monitors": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Monitors",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[contains(@onclick,'monitor')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/"
+	},
+	"Sony_vaio_i5": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Sony vaio i5",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//h4/a[contains(text(),'Sony vaio i5')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/"
+	},
+	"Add_to_cart": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_WidgetWin_1",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Add to cart",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[contains(@onclick,'addToCart')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/prod.html?idp_=8"
+	},
+	"PRODUCT_STORE": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "PRODUCT STORE",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[@id='nava' and @href='index.html']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/prod.html?idp_=8"
+	},
+	"Next": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Next",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//li[2]/button",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/index.html"
+	},
+	"Previous": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Previous",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//li[1]/button",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/index.html"
+	},
+	"Dell_i7_8gb": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Dell i7 8gb",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//h4/a[contains(text(),'Dell i7 8gb')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/index.html"
+	},
+	"Cart": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Cart",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//a[@id='cartur' and @href='cart.html']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/prod.html?idp_=12"
+	},
+	"Delete": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Link",
+		"object_name": "Delete",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//tr[@class='success']/td[text()='Dell i7 8gb']/following-sibling::td/a[@href='#']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html"
+	},
+	"Price": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "param:object_class",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_WidgetWin_1",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "360",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//h3[@id='totalp']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html"
+	},
+	"Place_Order": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Place Order",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div[2]/button[@type='button']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Name": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Name",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='name' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Country": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Country:",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='country' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"City": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "City:",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='city' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Credit_card": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Credit card:",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='card' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Month": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Month:",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='month' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Year": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Text",
+		"object_name": "Year:",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//input[@id='year' and @type='text']",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Purchase": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "Purchase",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div[@id='orderModal' and @role='dialog']/div[@role='document']/div/div[3]/button[@type='button' and position()=2]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Thank_you": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "Thank you",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div[10]/h2",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"Order_price": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "Order_price",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div/p[contains(@class,'lead')]/text()[2][substring-after(.,' ')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"OK": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "",
+		"object_text": "",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Button",
+		"object_name": "OK",
+		"ignore_object_name": true,
+		"object_class": "",
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div[7]/div/button",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	},
+	"OrderIDsAndAmounts": {
+		"locations": [
+			{
+				"locator_name": "Spreadsheet",
+				"location": {
+					"path": "param:path"
+				}
+			}
+		],
+		"window_name": "Spreadsheets",
+		"version": 0,
+		"ignore_object_name": true,
+		"object_type": "Spreadsheet",
+		"object_flavor": "Table",
+		"object_library": "Spreadsheet",
+		"path": "%WORKDIR%\\OrderIDsAndAmounts.xlsx"
+	},
+	"Order_Id": {
+		"locations": [
+			{
+				"locator_name": "HTML",
+				"location": {
+					"xpath": "param:xpath",
+					"url": "param:url",
+					"title": "param:title"
+				}
+			}
+		],
+		"window_class": "Chrome_WidgetWin_1",
+		"object_text": "param:object_name",
+		"object_role": "ROLE_SYSTEM_WINDOW",
+		"object_class": "Chrome_RenderWidgetHostHWND",
+		"version": 0,
+		"object_type": "HTMLObject",
+		"object_flavor": "Generic",
+		"object_name": "Order_price",
+		"ignore_object_name": true,
+		"object_library": "Chrome HTML",
+		"window_name": "STORE",
+		"xpath": "//div/p[contains(@class,'lead')]/text()[1][substring-after(.,' ')]",
+		"title": "STORE",
+		"url": "https://www.demoblaze.com/cart.html#"
+	}
+};
